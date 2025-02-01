56ed51ef413147b447c90146f9a1364eb305d930
unknown <josutic.sutic@gmail.com>
2025-02-01 11:54:47 +0100

    feat(86c1wh5gr): rmeove public build

--- public/build/_shared/client-W7NL77HM.js
+++
@@ -1,8 +0,0 @@
-import {
-  require_client
-} from "/build/_shared/chunk-EEGQUSNY.js";
-import "/build/_shared/chunk-KKFS2OLB.js";
-import "/build/_shared/chunk-5XFDQ4ME.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export default require_client();
-//# sourceMappingURL=/build/_shared/client-W7NL77HM.js.map
--- public/build/_shared/react-RGF4BR3R.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react
-} from "/build/_shared/chunk-5XFDQ4ME.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export default require_react();
-//# sourceMappingURL=/build/_shared/react-RGF4BR3R.js.map
