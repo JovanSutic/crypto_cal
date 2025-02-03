340ceae00a6eb58542ec344953c546529e991477
unknown <josutic.sutic@gmail.com>
2025-02-03 18:58:50 +0100

    feat(86c1wh5qz): add chart and list

--- app/components/HeaderSelector.tsx
+++ app/components/HeaderSelector.tsx
@@ -9,21 +9,21 @@ function HeaderSelector() {
   const [city, setCity] = useState<string>("Belgrade");
   return (
     <div>
-      <div className="grid grid-cols-1 mb:grid-cols-5 gap-3 md:gap-4 mb-3 md:mb-6">
+      <div className="grid grid-cols-1 mb:grid-cols-5 gap-3 md:gap-4 mb-3 md:mb-6 border-box">
         <div className="col-span-1 md:col-span-2">
           <div className="w-full flex flex-row justify-center">
-            <div className="flex flex-col items-center px-4 py-1 rounded-lg bg-emerald-200">
-              <p className="font-bold text-zinc-600 text-[12px]">
+            <div className="flex flex-col items-center px-4 py-1 rounded-lg bg-slate-100 overflow-hidden">
+              <p className="font-bold text-slate-600 text-[12px]">
                 {`${coin} CURRENT PRICE`}
               </p>
-              <div className="text-3xl text-zinc-800 font-bold">
+              <div className="text-3xl md:text-2xl text-slate-800 font-bold">
                 $99,123.00
               </div>
             </div>
           </div>
         </div>
         <div className="col-span-1 md:col-span-3 md:col-start-3 flex-col">
-          <div className="w-full flex flex-row gap-1 md:gap-2 mb-3 bg-zinc-100 py-2 px-1 md:px-3 lg:px-4 rounded-lg">
+          <div className="w-full flex flex-row gap-1 md:gap-2 mb-3 bg-slate-50 py-2 px-1 md:px-3 lg:px-4 rounded-lg">
             {coins.map((item) => (
               <CoinButton
                 key={item.name}
